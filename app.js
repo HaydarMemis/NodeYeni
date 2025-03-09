b9096f69a5a93ff41ed5499f1bb422bf85f72640
Haydar Memis <haydarmemiiss@gmail.com>
2025-03-10 01:21:56 +0300

    add css

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 
 var fs = require('fs');
 var express = require('express');
-
+const path = require('path');
 var app = express();
+app.use('/public', express.static(path.join(__dirname,'public'))); //public klasorune erismek icin
 
 app.get('/', function(req,res){
   fs.readFile('index.html', function (err, data) {
