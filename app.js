967df9786e0d257d01727934985c7d16254a3c52
Haydar Memis <haydarmemiiss@gmail.com>
2025-03-10 01:27:19 +0300

    res.sendfile function

--- app.js
+++ app.js
@@ -5,28 +5,14 @@ var fs = require('fs');
 var express = require('express');
 const path = require('path');
 var app = express();
-app.use('/public', express.static(path.join(__dirname,'public'))); //public klasorune erismek icin
+app.use('/public', express.static(path.join(__dirname,'public'))); //public klasorune erismek icin video 4
 
 app.get('/', function(req,res){
-  fs.readFile('index.html', function (err, data) {
-
-    res.write(data);
-
-    res.end('mesaj bitti');
-
-
-  });
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/login', function(req,res){
-  fs.readFile('login.html', function (err, data) {
-
-    res.write(data);
-
-    res.end('mesaj bitti');
-
-
-  });
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 app.listen(8000);
