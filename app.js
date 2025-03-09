fb872d3e616e2d3f9c9539417f22622226d373f0
Haydar Memis <haydarmemiiss@gmail.com>
2025-03-10 01:33:58 +0300

    req and res separated

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ var fs = require('fs');
 var express = require('express');
 const path = require('path');
 var app = express();
-app.use('/public', express.static(path.join(__dirname,'public'))); //public klasorune erismek icin video 4
+app.use('/public', express.static(path.join(__dirname, 'public'))); //public klasorune erismek icin video 4
 
-app.get('/', function(req,res){
+var indexController = function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/login', function(req,res){
+}
+var loginController = function (req, res) {
   res.sendFile(path.join(__dirname, 'login.html'));
-});
+}
+
+app.get('/', indexController);
+
+app.get('/login', loginController);
 
 app.listen(8000);
