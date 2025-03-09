e8c19a0c6ca96155451aed773ebb1f447cca42f8
Haydar Memis <haydarmemiiss@gmail.com>
2025-03-09 04:43:46 +0300

    install express module

--- app.js
+++ app.js
@@ -6,15 +6,37 @@ var fs = require('fs');
 
 var server = http.createServer(function(reg,res){
 
-  fs.readFile('index.html',function(err,data){
+
+  if(reg.url == '/'){
+    fs.readFile('index.html',function(err,data){
 
 
-    res.write(data);
+      res.write(data);
+  
+      res.end('mesaj bitti');
+  
+  
+    });
+  }
+  
+  if(reg.url == '/login'){
+    fs.readFile('login.html',function(err,data){
 
-    res.end('mesaj bitti');
+
+      res.write(data);
+
+      res.write('deneme');
+  
+      res.end('mesaj bitti');
+  
+  
+    });
+  }
 
 
-  });
+  console.log(reg.url);
+
+ 
 
   
 
